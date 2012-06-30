DUT-ShiLongYu/dart

commit b2e08bbeb6ffbb92b985fcc0a5ee223f87fcc500

modified biped

src/apps/biped/Controller.cpp
@@ -4,6 +4,7 @@
 #include "kinematics/BodyNode.h"
 #include "kinematics/Dof.h"
 #include "kinematics/Joint.h"
+#include "Kinematics/Shape.h"
 
 using namespace kinematics;
 using namespace Eigen;
@@ -36,30 +37,30 @@ Controller::Controller(dynamics::SkeletonDynamics *_skel, double _t) {
 
     // hips
     mKp(6, 6) = 500.0;
-    mKd(6, 6) = 2 * sqrt(mKp(6, 6));
+    //    mKd(6, 6) = 0.5 * sqrt(mKp(6, 6));
     mKp(12, 12) = 500.0;
-    mKd(12, 12) = 2 * sqrt(mKp(12, 12));
+    //    mKd(12, 12) = 0.5 * sqrt(mKp(12, 12));
     
     // knees
     mKp(9, 9) = 500.0;
-    mKd(9, 9) = 2 * sqrt(mKp(9, 9));
+    //    mKd(9, 9) = 0.5 * sqrt(mKp(9, 9));
     mKp(15, 15) = 500.0;
-    mKd(15, 15) = 2 * sqrt(mKd(15, 15));
+    //    mKd(15, 15) = 0.5 * sqrt(mKd(15, 15));
     
     // ankles
     mKp(10, 10) = 500.0;
-    mKd(10, 10) = 2 * sqrt(mKp(10, 10));
+    //    mKd(10, 10) = 0.5 * sqrt(mKp(10, 10));
     mKp(11, 11) = 500.0;
     mKd(11, 11) = 2 * sqrt(mKp(11, 11));
     mKp(16, 16) = 500.0;
-    mKd(16, 16) = 2 * sqrt(mKp(16, 16));
+    mKd(16, 16) = 0.5 * sqrt(mKp(16, 16));
     mKp(17, 17) = 500.0;
     mKd(17, 17) = 2 * sqrt(mKp(17, 17));
     
     // lower back
-    //    mKp(18, 18) = 500.0;
+        mKp(18, 18) = 500.0;
     //    mKd(18, 18) = 2 * sqrt(mKp(18, 18));
-    //    mKp(19, 19) = 500.0;
+        mKp(19, 19) = 500.0;
     //    mKd(19, 19) = 2 * sqrt(mKp(19, 19));
 
     // mass tree is only used when SPD is not activated
@@ -105,25 +106,39 @@ void Controller::computeTorques(const Eigen::VectorXd& _dof, const Eigen::Vector
     */
 
     // virtual force on lower body
-    /*
-    Vector3d lHeel = lFoot->getLocalCOM(); //lFoot->getWorldTransform().block(0, 3, 3, 1);
-    Vector3d rHeel = rFoot->getLocalCOM(); //rFoot->getWorldTransform().block(0, 3, 3, 1);
+    Vector3d vf(0.0, 0.0, 0.0);
+    if (mFrame > 800 && mFrame < 850) {
+        vf[0] = -3000;
+        //vf[1] = 10000;
+    }
+
+    BodyNode *lFoot = mSkel->getNode("fullbody_h_foot_left");
+    BodyNode *rFoot = mSkel->getNode("fullbody_h_foot_right");
+    double footLen = lFoot->getShape()->getDim().maxCoeff();
+    Vector3d lHeel =  utils::xformHom(lFoot->getWorldTransform(), Vector3d(footLen, 0, 0));
+    Vector3d rHeel =  utils::xformHom(rFoot->getWorldTransform(), Vector3d(footLen, 0, 0));
 
     for (int i = 0; i < lFoot->getNumDependentDofs(); i++) {
         int index = lFoot->getDependentDof(i);
         VectorXd jCol = utils::xformHom(lFoot->getDerivWorldTransform(i), lHeel);
-        J.col(index) = jCol;
+        if (index == 9)
+            J.col(index) = -jCol;
+        else
+            J.col(index) = jCol;
     }
-    //mTorques += J.transpose() * vf / 2.0;
+    mTorques += J.transpose() * vf / 2.0;
         
     J.setZero();
     for (int i = 0; i < rFoot->getNumDependentDofs(); i++) {
         int index = rFoot->getDependentDof(i);
         VectorXd jCol = utils::xformHom(rFoot->getDerivWorldTransform(i), rHeel);
-        J.col(index) = jCol;
+        if (index == 15)
+            J.col(index) = -jCol;
+        else
+            J.col(index) = jCol;
     }
-    //mTorques += J.transpose() * vf / 2.0;
-    */
+    mTorques += J.transpose() * vf / 2.0;
+    
 
     // gravity compensation for upper body
     /*    
@@ -146,7 +161,7 @@ void Controller::computeTorques(const Eigen::VectorXd& _dof, const Eigen::Vector
     */
 
     // virtual force on COM
-      
+    /*
     Vector3d vf(0.0, 0.0, 0.0);
     Vector3d com = mSkel->getWorldCOM();
     BodyNode *lFoot = mSkel->getNode("fullbody_h_foot_left");
@@ -166,8 +181,8 @@ void Controller::computeTorques(const Eigen::VectorXd& _dof, const Eigen::Vector
         }
     }
     mTorques += J.transpose() / mSkel->getMass() * vf;
-    
-    /*
+    */
+    /*   
     // virtual force from ankles
     Vector3d vf(0.0, 0.0, 0.0);
     Vector3d com = mSkel->getWorldCOM();
@@ -176,10 +191,12 @@ void Controller::computeTorques(const Eigen::VectorXd& _dof, const Eigen::Vector
     Vector3d cp = (lFoot->evalWorldPos(Vector3d::Zero()) + rFoot->evalWorldPos(Vector3d::Zero())) /2.0;
     vf = (cp - com) * 1000;
     vf[0] += 18;
-    vf[1] = 0.0;
+    vf[1] = 0;
+    if (mFrame > 800 && mFrame < 850) {
+        vf[0] = 10000;
+        //vf[1] = 10000;
+    }
     J.setZero();
-    //    BodyNode *hand = mSkel->getNode("fullbody_h_hand_left");
-    //    BodyNode *abd = mSkel->getNode("fullbody_h_abdomen");
     BodyNode *node = mSkel->getNode("fullbody_h_spine");
     BodyNode *foot = mSkel->getNode("fullbody_h_foot_left");
     //    Vector3d handCOM = hand->getLocalCOM();
@@ -199,6 +216,7 @@ void Controller::computeTorques(const Eigen::VectorXd& _dof, const Eigen::Vector
         J.col(index) = jCol;
     }
     
+    //    node = mSkel->getNode("fullbody_h_scapula_right");
     foot = mSkel->getNode("fullbody_h_foot_right");
     for (int i = 0; i < foot->getNumDependentDofs(); i++) {
         int index = foot->getDependentDof(i);
@@ -213,9 +231,8 @@ void Controller::computeTorques(const Eigen::VectorXd& _dof, const Eigen::Vector
         VectorXd jCol = utils::xformHom(foot->getWorldTransform().transpose() * node->getDerivWorldTransform(i), nodeCOM);
         J.col(index) += jCol;
     }
-        //    vf[2] = -vf[2];
     mTorques += J.transpose() * vf;
-    */  
+    */
     // SPD control force
     for (int i = 0; i < nDof; i++)
         mTorques[i] += -mKp(i, i) * (_dof[i] + _dofVel[i] * mTimestep - mDesiredDofs[i]) - mSkel->getKd()(i, i) * _dofVel[i];

src/apps/biped/MyWindow.cpp
@@ -25,17 +25,7 @@ void MyWindow::initDyn()
         mDofs[i].setZero();
         mDofVels[i].setZero();
     }
-    /*
-    mDofs[0][1] = -1.88;
-    mDofs[1][3] = -1;
-    mDofs[1][19] = 1;
-    mDofs[1][6] = 1.4;
-    mDofs[1][12] = 1.4;
-    mDofs[1][9] = -0.5;
-    mDofs[1][10] = 0.1;
-    mDofs[1][15] = -0.5;
-    mDofs[1][16] = 0.1;
-    */
+    
     mDofs[0][1] = -0.9;
     mDofs[1][19] = -0.1;
     mDofs[1][6] = 0.1;
@@ -44,6 +34,15 @@ void MyWindow::initDyn()
     mDofs[1][12] = 0.1;
     mDofs[1][15] = -0.2;
     mDofs[1][16] = 0.1;    
+      
+    mDofs[1][6] = 0.5;
+    mDofs[1][9] = -0.8;
+    mDofs[1][10] = 0.3;
+    mDofs[1][12] = 0.5;
+    mDofs[1][15] = -0.8;
+    mDofs[1][16] = 0.3;
+    mDofs[1][19] = -0.3;
+    
 
     for (unsigned int i = 0; i < mSkels.size(); i++) {
         mSkels[i]->setPose(mDofs[i], false, false);
@@ -58,7 +57,7 @@ void MyWindow::initDyn()
 
     mCollisionHandle = new dynamics::ContactDynamics(mSkels, mTimeStep);
     mCollisionHandle->setSPDFlag(true);
-    mSkels[1]->setKd(MatrixXd::Identity(nDof, nDof) * 300); 
+    mSkels[1]->setKd(MatrixXd::Identity(nDof, nDof) * 100); 
 }
 
 VectorXd MyWindow::getState() {
@@ -109,7 +108,6 @@ void MyWindow::setPose() {
             mSkels[i]->setPose(mDofs[i], true, true);
             mSkels[i]->computeDynamics(mGravity, mDofVels[i], true);    
             mController->computeTorques(mDofs[1], mDofVels[1]);
-            //            mController->getSkel()->setInternalForces(mController->getTorques());
         }
     }
         mCollisionHandle->applyContactForces();
@@ -119,24 +117,22 @@ void MyWindow::displayTimer(int _val)
 {
     int numIter = mDisplayTimeout / (mTimeStep * 1000);
     if (mPlay) {
-        mPlayFrame += 8;
+        mPlayFrame += 30;
         if (mPlayFrame >= mBakedStates.size())
             mPlayFrame = 0;
         glutPostRedisplay();
         glutTimerFunc(mDisplayTimeout, refreshTimer, _val);        
     }else if (mSim) {
-        //    static Timer tSim("Simulation");
+        static Timer tSim("Simulation");
         for (int i = 0; i < numIter; i++) {
-            //        tSim.startTimer();
+            //            tSim.startTimer();
             static_cast<BodyNodeDynamics*>(mSkels[1]->getNode(8))->addExtForce(Vector3d(0.0, 0.0, 0.0), mForce);
             mIntegrator.integrate(this, mTimeStep);
-            //        tSim.stopTimer();
-
+            //            tSim.stopTimer();
+            //            tSim.printScreen();
             bake();
             cout << "iter " << i + mSimFrame << endl;
         }
-        //    tSim.printScreen();
-
 
         mImpulseDuration--;
          if (mImpulseDuration <= 0) {
@@ -149,7 +145,18 @@ void MyWindow::displayTimer(int _val)
 
         glutPostRedisplay();
         glutTimerFunc(mDisplayTimeout, refreshTimer, _val);
-        
+        /*
+        if (mSimFrame == numIter * 25) {
+            mController->setDesiredDof(6, -0.0);
+            mController->setDesiredDof(9, 0.0);
+            mController->setDesiredDof(10, -0.5);
+            mController->setDesiredDof(12, -0.0);
+            mController->setDesiredDof(15, 0.0);
+            mController->setDesiredDof(16, -0.5);
+            mController->setDesiredDof(19, -0.1);
+            }*/
+  
+        /*
         double forceMag = 55;
         if (mSimFrame == numIter * 50) {
             mForce[0] = forceMag;
@@ -177,7 +184,7 @@ void MyWindow::displayTimer(int _val)
             mForce[2] = forceMag;
             mImpulseDuration = 10.0;
         }
-        
+        */
     }
 }
 
@@ -200,11 +207,11 @@ void MyWindow::draw()
                 int nContact = (mBakedStates[mPlayFrame].size() - sumDofs) / 6;
                 for (int i = 0; i < nContact; i++) {
                     Vector3d v = mBakedStates[mPlayFrame].segment(sumDofs + i * 6, 3);
-                    Vector3d n = mBakedStates[mPlayFrame].segment(sumDofs + i * 6 + 3, 3) / 10.0;
+                    Vector3d f = mBakedStates[mPlayFrame].segment(sumDofs + i * 6 + 3, 3) / 100.0;
 
                     glBegin(GL_LINES);
                     glVertex3f(v[0], v[1], v[2]);
-                    glVertex3f(v[0] + n[0], v[1] + n[1], v[2] + n[2]);
+                    glVertex3f(v[0] + f[0], v[1] + f[1], v[2] + f[2]);
                     glEnd();
                     mRI->setPenColor(Vector3d(0.2, 0.2, 0.8));
                     mRI->pushMatrix();

src/apps/biped/MyWindow.h
@@ -22,7 +22,7 @@ namespace integration{
 class MyWindow : public yui::Win3D, public integration::IntegrableSystem {
 public:
     //    MyWindow(dynamics::SkeletonDynamics* _m1, dynamics::SkeletonDynamics* _m2)
-    MyWindow(dynamics::SkeletonDynamics* _mList = 0 ...): Win3D() {
+ MyWindow(dynamics::SkeletonDynamics* _mList = 0, ...): Win3D() {
         mBackground[0] = 1.0;
         mBackground[1] = 1.0;
         mBackground[2] = 1.0;

src/collision/collision_skeleton.h
@@ -70,8 +70,7 @@ namespace collision_checking {
         double penetrationDepth;
 
         bool isAdjacent(ContactPoint &otherPt){
-            return (bd1==otherPt.bd1&&triID1==otherPt.triID1||bd2==otherPt.bd2&&triID2==otherPt.triID2||
-                bd1==otherPt.bd2&&triID1==otherPt.triID2||bd2==otherPt.bd1&&triID2==otherPt.triID1);
+            return (((((((bd1==otherPt.bd1 && triID1==otherPt.triID1) || bd2==otherPt.bd2) && triID2==otherPt.triID2) || bd1==otherPt.bd2) && triID1==otherPt.triID2) || bd2==otherPt.bd1) && triID2==otherPt.triID1);
         }
     };
 

src/optimizer/snopt/SnoptInterface.cpp
@@ -353,7 +353,7 @@ namespace optimizer {
                 }else if(mConstrEqns[i] == 4){
                     bl[mNumCoef + i] = -1e-2 + x[mNumCoef + i];
                     bu[mNumCoef + i] = 1e-2 + x[mNumCoef + i];
-                }else if(mConstrEqns[i] = 5){
+                }else if(mConstrEqns[i] == 5){
                     bl[mNumCoef + i] = -1e3 + x[mNumCoef + i];
                     bu[mNumCoef + i] = 1e3 + x[mNumCoef + i];
                 }

commit a0511494fe62a3ee5475ce65541587800a659f22

Fix include path case bug, introduced in b2e08bb

src/apps/biped/Controller.cpp
@@ -4,7 +4,7 @@
 #include "kinematics/BodyNode.h"
 #include "kinematics/Dof.h"
 #include "kinematics/Joint.h"
-#include "Kinematics/Shape.h"
+#include "kinematics/Shape.h"
 
 using namespace kinematics;
 using namespace Eigen;