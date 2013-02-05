DUT-ShiLongYu/dart

commit c2f98d2bd1d75e0bc27d1c38c09bde88a7ef881b

Added mGeomTransform variable to Shape.h

src/kinematics/Shape.h
@@ -73,8 +73,12 @@ namespace kinematics {
         void setDim(const Eigen::Vector3d& _dim);
         inline Eigen::Vector3d getDim() const { return mDim; }
 
-				inline void setOffset(Eigen::Vector3d _offset) { mOffset = _offset; }
+	inline void setOffset(Eigen::Vector3d _offset) { mOffset = _offset; }
         inline Eigen::Vector3d getOffset() const { return mOffset; }
+
+	inline void setGeomTransform(Eigen::Matrix4d _geomTransf ) { mGeomTransform = _geomTransf; }
+        inline Eigen::Matrix4d getGeomTransform() const { return mGeomTransform; }
+
 
         void setMass(const double _m);
         inline double getMass() { return mMass; }
@@ -116,6 +120,8 @@ namespace kinematics {
         int mID; // unique id
         Eigen::Vector3d mColor;		///< color for the primitive
         Eigen::Vector3d mOffset; ///< the origin of this primitive in the bodynode frame>
+	Eigen::Matrix4d mGeomTransform; ///< Local Geometric transformation of the Shape w.r.t. parent frame
+
 
         const aiScene *mVizMesh; ///< mesh for visualization>
         const aiScene *mCollisionMesh; ///< mesh for collision detection>

src/robotics/parser/dart_parser/ToDartUtils.cpp
@@ -66,7 +66,7 @@ kinematics::Joint* DartLoader::createDartJoint( boost::shared_ptr<urdf::Joint> _
   dynamics::BodyNodeDynamics* parent = getNode( _jt->parent_link_name );
   dynamics::BodyNodeDynamics* child = getNode( _jt->child_link_name );
   if(debug) printf ("Joint: %s Parent: %s child : %s \n",   jointName, parent->getName(), 
-	 child->getName() );
+		    child->getName() );
   // Create joint
   kinematics::Joint* joint = new kinematics::Joint( parent,
 						    child,
@@ -77,7 +77,6 @@ kinematics::Joint* DartLoader::createDartJoint( boost::shared_ptr<urdf::Joint> _
   y = p.position.y;
   z = p.position.z;
   p.rotation.getRPY( roll, pitch, yaw );
-    
   add_XyzRpy( joint, x, y, z, roll, pitch, yaw );
   
   // Add DOF if prismatic or revolute joint
@@ -86,10 +85,24 @@ kinematics::Joint* DartLoader::createDartJoint( boost::shared_ptr<urdf::Joint> _
     
     // Revolute
     if(   _jt->type == urdf::Joint::REVOLUTE ) {  
-      if( _jt->axis.x == 1  || _jt->axis.x == -1 ) { axis = GOLEM_ROLL; }
-      else if( _jt->axis.y == 1 || _jt->axis.y == -1 ) { axis = GOLEM_PITCH; }
-      else if( _jt->axis.z == 1 || _jt->axis.z == -1  ) { axis = GOLEM_YAW; }
-      else { if(debug) printf ("[ERROR] No axis defined for Revolute DOF! \n"); }
+      if( _jt->axis.x == 1 ) {
+	axis = GOLEM_ROLL; printf("Define Joint Axis X \n");
+      } else if( _jt->axis.y == 1 ) {
+	axis = GOLEM_PITCH; printf("Define Joint Axis Y \n");
+      } else if( _jt->axis.z == 1 ) {
+	axis = GOLEM_YAW; printf("Define Joint Axis Z \n");
+      } /*else if( _jt->axis.x == -1 ) {
+  
+      } else if( _jt->axis.y == -1 ) { 
+
+      }
+      else if(  _jt->axis.z == -1  ) { 
+	axis = GOLEM_YAW; 
+	}*/
+      else { 
+	//if(debug) 
+	printf ("[ERROR] No axis defined for Revolute DOF! (no X, Y, Z positive at least) \n"); 
+      }
     }
     // Prismatic
     else if( _jt->type == urdf::Joint::PRISMATIC ) {
@@ -97,7 +110,7 @@ kinematics::Joint* DartLoader::createDartJoint( boost::shared_ptr<urdf::Joint> _
       if( _jt->axis.x == 1 || _jt->axis.x == -1  ) { axis = GOLEM_X; }
       else if( _jt->axis.y == 1 || _jt->axis.y == -1  ) { axis = GOLEM_Y; }
       else if( _jt->axis.z == 1 || _jt->axis.z == -1 ) { axis = GOLEM_Z; }
-      else { if(debug) printf (" [ERROR] No axis defined for Prismatic DOF! \n"); }
+      else { printf (" [ERROR] No axis defined for Prismatic DOF! \n"); }
     }
     
     vmin = _jt->limits->lower;
@@ -108,12 +121,13 @@ kinematics::Joint* DartLoader::createDartJoint( boost::shared_ptr<urdf::Joint> _
   
   // Fixed, do not add DOF
   else if( _jt->type == urdf::Joint::FIXED ) {
-    if(debug) printf ("Fixed joint: %s \n", jointName );
+    //if(debug) 
+      printf ("Fixed joint: %s \n", jointName );
   }
   
   // None of the above
   else {
-    if(debug) printf ("[createDartJoint] ERROR: Parsing %s joint: No PRISMATIC or REVOLUTE or FIXED \n", jointName );
+    printf ("[createDartJoint] ERROR: Parsing %s joint: No PRISMATIC or REVOLUTE or FIXED \n", jointName );
   }
   
   return joint;
@@ -166,8 +180,8 @@ dynamics::BodyNodeDynamics* DartLoader::createDartNode( boost::shared_ptr<urdf::
       // Set it to Node
       node->setLocalCOM( localCOM );
 
-    if(debug) printf ("* Mass is: %f \n", mass);
-    std::cout<< "* Inertia is: \n"<< inertia << std::endl;
+      if( debug ) { printf ("* Mass is: %f \n", mass); }
+      if( debug ) { std::cout<< "* Inertia is: \n"<< inertia << std::endl; }
   }
   
   if( !_lk->visual ) {

commit 010bfc87a1fb4a093230268ecec7c8295f867c57

Fix Eigen alignment issue introduced in commit c2f98d2bd1d75e0bc27d1c38c09bde88a7ef881b

src/kinematics/Shape.cpp
@@ -52,7 +52,7 @@ namespace kinematics {
           mInertia(Matrix3d::Zero()),
           mMassTensor(Matrix4d::Zero()),
           mOffset(0, 0, 0),
-	  mVisTransform( Matrix4d::Identity() ),
+          mVisTransform(Matrix4d::Identity()),
           mVizMesh(NULL),
           mCollisionMesh(NULL),
           mID(mCounter++),

src/kinematics/Shape.h
@@ -76,7 +76,7 @@ namespace kinematics {
 	inline void setOffset(Eigen::Vector3d _offset) { mOffset = _offset; }
         inline Eigen::Vector3d getOffset() const { return mOffset; }
 
-	inline void setVisTransform(Eigen::Matrix4d _visTransf ) { mVisTransform = _visTransf; }
+        inline void setVisTransform(const Eigen::Matrix4d &_visTransform ) { mVisTransform = _visTransform; }
         inline Eigen::Matrix4d getVisTransform() const { return mVisTransform; }
 
 