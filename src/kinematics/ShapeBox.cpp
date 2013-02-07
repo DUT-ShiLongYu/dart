DUT-ShiLongYu/dart

commit d64c12a1bf6e1ffe4ec60a0c633c1c374df60270

Fix #27: Remove unused Shape::mMassTensor

src/kinematics/ShapeBox.cpp
@@ -51,9 +51,7 @@ namespace kinematics {
         if (_dim != Vector3d::Zero())
             computeVolume();
         if (mMass != 0){
-            computeMassTensor();
-            computeInertiaFromMassTensor();
-            computeVolume();
+            computeInertia();
         }
     }
 
@@ -68,12 +66,11 @@ namespace kinematics {
         _ri->popMatrix();
     }
 
-    void ShapeBox::computeMassTensor() {
-        mMassTensor(0, 0) = (mDim(0)*mDim(0))/12;
-        mMassTensor(1, 1) = (mDim(1)*mDim(1))/12;
-        mMassTensor(2, 2) = (mDim(2)*mDim(2))/12;
-        mMassTensor(3, 3) = 1;
-        mMassTensor *= mMass;
+    void ShapeBox::computeInertia() {
+        mInertia = Matrix3d::Zero();
+        mInertia(0, 0) = mMass / 12.0 * (mDim(1) * mDim(1) + mDim(2) * mDim(2));
+        mInertia(1, 1) = mMass / 12.0 * (mDim(0) * mDim(0) + mDim(2) * mDim(2));
+        mInertia(1, 1) = mMass / 12.0 * (mDim(0) * mDim(0) + mDim(1) * mDim(1));
     }
 
     void ShapeBox::computeVolume() {

src/kinematics/ShapeEllipsoid.cpp
@@ -50,8 +50,7 @@ namespace kinematics {
         if (mDim != Vector3d::Zero())
             computeVolume();
         if (mMass != 0){
-            computeMassTensor();
-            computeInertiaFromMassTensor();
+            computeInertia();
         }
     }
 
@@ -67,12 +66,11 @@ namespace kinematics {
         _ri->popMatrix();
     }
 
-    void ShapeEllipsoid::computeMassTensor() {
-        mMassTensor(0, 0) = (mDim(0)*mDim(0))/20;
-        mMassTensor(1, 1) = (mDim(1)*mDim(1))/20;
-        mMassTensor(2, 2) = (mDim(2)*mDim(2))/20;
-        mMassTensor(3, 3) = 1;
-        mMassTensor *= mMass;
+    void ShapeEllipsoid::computeInertia() {
+        mInertia = Matrix3d::Zero();
+        mInertia(0, 0) = mMass / 20.0 * (mDim(1) * mDim(1) + mDim(2) * mDim(2));
+        mInertia(1, 1) = mMass / 20.0 * (mDim(0) * mDim(0) + mDim(2) * mDim(2));
+        mInertia(1, 1) = mMass / 20.0 * (mDim(0) * mDim(0) + mDim(1) * mDim(1));
     }
 
     void ShapeEllipsoid::computeVolume() {

src/kinematics/ShapeMesh.cpp
@@ -56,8 +56,7 @@ namespace kinematics {
         if (mDim != Vector3d::Zero())
             computeVolume();
         if (mMass != 0){
-            computeMassTensor();
-            computeInertiaFromMassTensor();
+            computeInertia();
         }
     }
 
@@ -73,7 +72,7 @@ namespace kinematics {
         _ri->popMatrix();
     }
 
-    void ShapeMesh::computeMassTensor() {
+    void ShapeMesh::computeInertia() {
         // use bounding box to represent the mesh
         double max_X = -1e7;
         double max_Y = -1e7;
@@ -103,11 +102,10 @@ namespace kinematics {
         double w = mDim[2] * (max_Z - min_Z);
 
 
-        mMassTensor(0, 0) = l * l / 12;
-        mMassTensor(1, 1) = h * h / 12;
-        mMassTensor(2, 2) = w * w / 12;
-        mMassTensor(3, 3) = 1;
-        mMassTensor *= mMass;
+        mInertia = Matrix3d::Zero();
+        mInertia(0, 0) = mMass / 12.0 * (h * h + w * w);
+        mInertia(1, 1) = mMass / 12.0 * (l * l + w * w);
+        mInertia(1, 1) = mMass / 12.0 * (l * l + h * h);
     }
 
     void ShapeMesh::computeVolume() {

src/kinematics/Shape.cpp
@@ -50,7 +50,6 @@ namespace kinematics {
           mMass(_mass),
           mVolume(0), 
           mInertia(Matrix3d::Zero()),
-          mMassTensor(Matrix4d::Zero()),
           mOffset(0, 0, 0),
           mVisTransform(Matrix4d::Identity()),
           mVizMesh(NULL),
@@ -61,47 +60,18 @@ namespace kinematics {
 
     void Shape::setInertia(const Eigen::Matrix3d& _inertia) {
         mInertia = _inertia;
-        setMassTensorFromInertia();
     }
 
     void Shape::setDim(const Eigen::Vector3d& _dim) {
         mDim = _dim;
         computeVolume();
-        computeMassTensor();
-        computeInertiaFromMassTensor();
+        computeInertia();
     }
 
     void Shape::setMass(double _m) {
         mMass = _m;
-        computeMassTensor();
-        computeInertiaFromMassTensor();
+        computeInertia();
     }
 
     int Shape::mCounter = PRIMITIVE_MAGIC_NUMBER;
-
-    void Shape::setMassTensorFromInertia() {
-        // compute the half trace of mat = row*integral((x*x+y*y+z*z)dxdydz) = sum of moment of inertia along all 3 axes
-	
-        double halftrace = 0.5* mInertia.trace();
-        for(int i=0; i<3; i++) {
-            for(int j=0; j<3; j++) {
-                mMassTensor(i, j) = -mInertia(i, j);
-            }
-        }
-        mMassTensor(0, 0) += halftrace;
-        mMassTensor(1, 1) += halftrace;
-        mMassTensor(2, 2) += halftrace;
-        mMassTensor(3, 3) = mMass;
-    }
-
-    void Shape::computeInertiaFromMassTensor(){
-        double trace = mMassTensor(0, 0) + mMassTensor(1, 1) + mMassTensor(2, 2);
-        for(int i=0; i<3; i++)
-            for(int j=0; j<3; j++)
-                mInertia(i, j) = -mMassTensor(i, j);
-        mInertia(0, 0) += trace;
-        mInertia(1, 1) += trace;
-        mInertia(2, 2) += trace;
-    }
-
 } // namespace kinematics

src/kinematics/Shape.h
@@ -65,7 +65,6 @@ namespace kinematics {
         
         void setInertia(const Eigen::Matrix3d& _inertia);
         inline Eigen::Matrix3d getInertia() const { return mInertia; }
-        inline Eigen::Matrix4d getMassTensor() const { return mMassTensor; }
 
         inline void setColor(const Eigen::Vector3d& _color) { mColor = _color; }
         inline Eigen::Vector3d getColor() const { return mColor; }
@@ -98,13 +97,7 @@ namespace kinematics {
         virtual void draw(renderer::RenderInterface* _ri = NULL, const Eigen::Vector4d& _color=Eigen::Vector4d::Ones(), bool _useDefaultColor = true) const {}
 
     protected:
-        void setMassTensorFromInertia();    ///< sets the "mass tensor" in lagrangian dynamics from the inertia matrix
-        void computeInertiaFromMassTensor();    ///< computes the inertia matrix from the "mass tensor" in lagrangian dynamics
-        virtual void computeMassTensor() {}
-        inline virtual void computeInertia() {
-            computeMassTensor();
-            computeInertiaFromMassTensor();
-        }
+        virtual void computeInertia() {}
         virtual void computeVolume() {}
 
         virtual void initMeshes() {}
@@ -115,7 +108,6 @@ namespace kinematics {
         double mVolume; ///< volume enclosed by the geometry
 
         Eigen::Matrix3d mInertia;	///< inertia matrix
-        Eigen::Matrix4d mMassTensor; ///< homogenous mass tensor for lagrangian dynamics
 
         int mID; // unique id
         Eigen::Vector3d mColor;		///< color for the primitive

src/kinematics/ShapeBox.h
@@ -49,7 +49,7 @@ namespace kinematics {
 
         void draw(renderer::RenderInterface* _ri = NULL, const Eigen::Vector4d& _col=Eigen::Vector4d::Ones(), bool _default = true) const;
     private:
-        void computeMassTensor();
+        void computeInertia();
         void computeVolume();
         
     public:

src/kinematics/ShapeEllipsoid.h
@@ -48,7 +48,7 @@ namespace kinematics {
 
         void draw(renderer::RenderInterface* _ri = NULL, const Eigen::Vector4d& _col=Eigen::Vector4d::Ones(), bool _useDefaultColor = true) const;
     private:
-        void computeMassTensor();
+        void computeInertia();
         void computeVolume();
         void initMeshes();
     public:

src/kinematics/ShapeMesh.h
@@ -49,7 +49,7 @@ namespace kinematics {
         void draw(renderer::RenderInterface* _ri = NULL, const Eigen::Vector4d& _col = Eigen::Vector4d::Ones(), bool _default = true) const;
         static const aiScene* loadMesh(const std::string& fileName);
     private:
-        void computeMassTensor();
+        void computeInertia();
         void computeVolume();
         void initMeshes(); ///< initialize mVizMesh and mCollisionMesh>
 

commit 7b26494881b27fd1f86e3bef198a39101e838025

Fix bug in inertia calculation introduced in d64c12a1bf6e1ffe4ec60a0c633c1c374df60270

src/kinematics/ShapeBox.cpp
@@ -70,7 +70,7 @@ namespace kinematics {
         mInertia = Matrix3d::Zero();
         mInertia(0, 0) = mMass / 12.0 * (mDim(1) * mDim(1) + mDim(2) * mDim(2));
         mInertia(1, 1) = mMass / 12.0 * (mDim(0) * mDim(0) + mDim(2) * mDim(2));
-        mInertia(1, 1) = mMass / 12.0 * (mDim(0) * mDim(0) + mDim(1) * mDim(1));
+        mInertia(2, 2) = mMass / 12.0 * (mDim(0) * mDim(0) + mDim(1) * mDim(1));
     }
 
     void ShapeBox::computeVolume() {

src/kinematics/ShapeEllipsoid.cpp
@@ -70,7 +70,7 @@ namespace kinematics {
         mInertia = Matrix3d::Zero();
         mInertia(0, 0) = mMass / 20.0 * (mDim(1) * mDim(1) + mDim(2) * mDim(2));
         mInertia(1, 1) = mMass / 20.0 * (mDim(0) * mDim(0) + mDim(2) * mDim(2));
-        mInertia(1, 1) = mMass / 20.0 * (mDim(0) * mDim(0) + mDim(1) * mDim(1));
+        mInertia(2, 2) = mMass / 20.0 * (mDim(0) * mDim(0) + mDim(1) * mDim(1));
     }
 
     void ShapeEllipsoid::computeVolume() {

src/kinematics/ShapeMesh.cpp
@@ -105,7 +105,7 @@ namespace kinematics {
         mInertia = Matrix3d::Zero();
         mInertia(0, 0) = mMass / 12.0 * (h * h + w * w);
         mInertia(1, 1) = mMass / 12.0 * (l * l + w * w);
-        mInertia(1, 1) = mMass / 12.0 * (l * l + h * h);
+        mInertia(2, 2) = mMass / 12.0 * (l * l + h * h);
     }
 
     void ShapeMesh::computeVolume() {