DUT-ShiLongYu/dart

commit 5136748a062d025092a2d9268ee00efafed3d5bf

Fix problem with rotated meshes.
The problem was that assimp rotates collada files such that the up-axis (specified in the collada file) aligns with assimp's y-axis.

src/kinematics/ShapeMesh.cpp
@@ -121,10 +121,18 @@ namespace kinematics {
                                                                               aiProcess_Triangulate            |
                                                                               aiProcess_JoinIdenticalVertices  |
                                                                               aiProcess_SortByPType            |
-                                                                              aiProcess_PreTransformVertices   |
                                                                               aiProcess_OptimizeMeshes,
                                                             NULL, propertyStore);
         aiReleasePropertyStore(propertyStore);
+
+        // Assimp rotates collada files such that the up-axis (specified in the collada file) aligns with assimp's y-axis.
+        // Here we are reverting this rotation.
+        // We are only catching files with the .dae file ending here. We might miss files with an .xml file ending,
+        // which would need to be looked into to figure out whether they are collada files.
+        if(fileName.length() >= 4 && fileName.substr(fileName.length() - 4, 4) == ".dae") {
+            scene->mRootNode->mTransformation = aiMatrix4x4();
+        }
+
         return scene;
     }
 } // namespace kinematics

commit 67e8cbd75a90bf20e13f2e777b807837234291c5

Fix collision checking problem caused by 5136748a062d025092a2d9268ee00efafed3d5bf

src/kinematics/ShapeMesh.cpp
@@ -132,6 +132,7 @@ namespace kinematics {
         if(fileName.length() >= 4 && fileName.substr(fileName.length() - 4, 4) == ".dae") {
             scene->mRootNode->mTransformation = aiMatrix4x4();
         }
+        scene = aiApplyPostProcessing(scene, aiProcess_PreTransformVertices);
 
         return scene;
     }