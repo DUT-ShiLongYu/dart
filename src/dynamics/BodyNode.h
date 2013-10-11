DUT-ShiLongYu/dart

commit 01362db6d0bad3ddc1e9b7a7b2445531f33b2a99

Add contact forces to each body node as external forces. This is for correct joint force/torque sensoring.
This is not a long term solution.

src/dynamics/BodyNode.h
@@ -327,6 +327,18 @@ public:
     void clearExternalForces();
 
     /// @brief
+    void addContactForce(const Eigen::Vector6d& _contactForce);
+
+    /// @brief
+    int getNumContactForces() const;
+
+    /// @brief
+    const Eigen::Vector6d& getContactForce(int _idx);
+
+    /// @brief
+    void clearContactForces();
+
+    /// @brief
     const Eigen::Vector6d& getExternalForceLocal() const;
 
     /// @brief
@@ -530,6 +542,9 @@ protected:
 
     /// @brief
     Eigen::MatrixXd mM;
+
+    /// @brief
+    std::vector<Eigen::Vector6d> mContactForces;
 
 private:
     void _updateGeralizedInertia();

src/constraint/ConstraintDynamics.cpp
@@ -583,6 +583,19 @@ void ConstraintDynamics::applySolutionODE() {
             Contact& contact = mCollisionDetector->getContact(i);
             contact.force.noalias() = getTangentBasisMatrixODE(contact.point, contact.normal) * f_d.segment(i * 2, 2);
             contact.force.noalias() += contact.normal * f_n[i];
+
+            // Add contact force to body nodes
+            Eigen::Isometry3d T = Eigen::Isometry3d::Identity();
+            Eigen::Vector6d F = Eigen::Vector6d::Zero();
+            T.translation() = contact.collisionNode1->getBodyNode()->getWorldTransform().inverse() * contact.point;
+            F.tail<3>() = contact.collisionNode1->getBodyNode()->getWorldTransform().linear().transpose() * contact.force;
+            Eigen::Vector6d contactForce = math::dAdInvT(T, F);
+            contact.collisionNode1->getBodyNode()->addContactForce(contactForce);
+
+            T.translation() = contact.collisionNode2->getBodyNode()->getWorldTransform().inverse() * contact.point;
+            F.tail<3>() = contact.collisionNode2->getBodyNode()->getWorldTransform().linear().transpose() * (-contact.force);
+            contactForce = math::dAdInvT(T, F);
+            contact.collisionNode2->getBodyNode()->addContactForce(contactForce);
         }
     }
     for (int i = 0; i < mLimitingDofIndex.size(); i++) {

src/dynamics/BodyNode.cpp
@@ -678,7 +678,29 @@ Eigen::Vector6d BodyNode::getExternalForceGlobal() const
     return math::dAdInvT(mW, mFext);
 }
 
-const Eigen::Vector6d&BodyNode::getBodyForce() const
+void BodyNode::addContactForce(const Eigen::Vector6d& _contactForce)
+{
+    mContactForces.push_back(_contactForce);
+}
+
+int BodyNode::getNumContactForces() const
+{
+    return mContactForces.size();
+}
+
+const Eigen::Vector6d& BodyNode::getContactForce(int _idx)
+{
+    assert(0 <= _idx && _idx < mContactForces.size());
+
+    return mContactForces[_idx];
+}
+
+void BodyNode::clearContactForces()
+{
+    mContactForces.clear();
+}
+
+const Eigen::Vector6d& BodyNode::getBodyForce() const
 {
     return mF;
 }
@@ -762,6 +784,9 @@ void BodyNode::updateBiasForce(const Eigen::Vector3d& _gravity)
 
     mB = -math::dad(mV, mI*mV) - mFext - mFgravity;
 
+    for (int i = 0; i < mContactForces.size(); ++i)
+        mB -= mContactForces[i];
+
     std::vector<BodyNode*>::iterator it;
     for (it = mChildBodyNodes.begin(); it != mChildBodyNodes.end(); ++it)
         mB += math::dAdInvT((*it)->getParentJoint()->getLocalTransform(),
@@ -792,16 +817,8 @@ void BodyNode::updateBeta()
 {
     mAlpha           = mParentJoint->get_tau();
 
-    // TODO: Need to find more efficient way in architecture
-    // Add constraint force
     if (mParentJoint->getNumGenCoords() > 0)
-    {
         mAlpha          += mParentJoint->getDampingForces();
-        Eigen::VectorXd Fc = Eigen::VectorXd::Zero(mParentJoint->getNumGenCoords());
-        for (int i = 0; i < mParentJoint->getNumGenCoords(); i++)
-            Fc(i) = mSkeleton->getConstraintForces()[mParentJoint->getGenCoord(i)->getSkeletonIndex()];
-        mAlpha          += Fc;
-    }
 
     mAlpha          -= mParentJoint->getLocalJacobian().transpose() *
                        (mAI*mEta + mB);
@@ -903,6 +920,7 @@ void BodyNode::_updateGeralizedInertia()
 void BodyNode::clearExternalForces()
 {
     mFext.setZero();
+    mContactForces.clear();
 }
 
 } // namespace dynamics

src/simulation/World.cpp
@@ -106,6 +106,7 @@ void World::setControlInput()
     }
 }
 
+#define USE_FEATHERSTON 1
 Eigen::VectorXd World::evalDeriv()
 {
     // Calculate M(q), M^{-1}(q)
@@ -127,8 +128,14 @@ Eigen::VectorXd World::evalDeriv()
         if (!mSkeletons[i]->isMobile())
             continue;
 
+#ifdef USE_FEATHERSTON
+        mSkeletons[i]->setConstraintForces(
+                    mConstraintHandler->getTotalConstraintForce(i) -
+                    mConstraintHandler->getContactForce(i));
+#else
         mSkeletons[i]->setConstraintForces(
                     mConstraintHandler->getTotalConstraintForce(i));
+#endif
     }
 
     // compute forward dynamics
@@ -136,8 +143,11 @@ Eigen::VectorXd World::evalDeriv()
          itrSkeleton != mSkeletons.end();
          ++itrSkeleton)
     {
-        //(*itrSkeleton)->computeForwardDynamicsID(mGravity);
+#ifdef USE_FEATHERSTON
         (*itrSkeleton)->computeForwardDynamicsFS(mGravity);
+#else
+        (*itrSkeleton)->computeForwardDynamicsID(mGravity);
+#endif
     }
 
     // compute derivatives for integration

commit 71111bde28b64a0ec36466c5655e88149a41f578

Fix alignment bug introduced in 01362db6d0bad3ddc1e9b7a7b2445531f33b2a99

src/dynamics/BodyNode.h
@@ -73,7 +73,7 @@ Runge-Kutta and fourth-order Runge Kutta.
 #ifndef DART_DYNAMICS_BODYNODE_H
 #define DART_DYNAMICS_BODYNODE_H
 
-#include <vector>
+#include <Eigen/StdVector>
 
 #include <Eigen/Dense>
 
@@ -544,7 +544,7 @@ protected:
     Eigen::MatrixXd mM;
 
     /// @brief
-    std::vector<Eigen::Vector6d> mContactForces;
+    std::vector<Eigen::Vector6d, Eigen::aligned_allocator<Eigen::Vector6d> > mContactForces;
 
 private:
     void _updateGeralizedInertia();